c340dee9e6c14a42eaae2cb3499bf1cd265f4c5c
chan <34330230+uagen@users.noreply.github.com>
2025-02-02 03:43:01 +0800

    Create SecurityConfig.java

---
+++ src/main/java/com/creativecube/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/vendor/**").hasRole("VENDOR")
+                .antMatchers("/client/**").hasRole("CLIENT")
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+}
